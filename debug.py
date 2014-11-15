4c5e6d84ebfccdd457e2735d29d62a86482d6d8e
isaacphi <isaac.phil@gmail.com>
2014-11-15 18:52:59 -0500

    fix debug

--- debug.py
+++ debug.py
@@ -8,14 +8,14 @@ ser = control.setup()
 # Main loop
 while True:
     next = input('Q/W/A/S for in, E/R/D/F for out ')
-    if next == 'Q': speeds = str(0) + '0,0,0\n'
-    if next == 'W': speeds = '0,' + str(180) + ',0,0\n'
-    if next == 'A': speeds = '0,0,' + str(0) + '0\n'
-    if next == 'S': speeds = '0,0,0,' + str(180) + '\n'
-    if next == 'E': speeds = str(180) + '0,0,0\n'
-    if next == 'R': speeds = '0,' + str(0) + ',0,0\n'
-    if next == 'D': speeds = '0,0,' + str(180) + '0\n'
-    if next == 'F': speeds = '0,0,0,' +str(0) + '\n'
-
-    time.sleep(0.5)
+    if next == 'Q': speeds = str(0) + '90,90,90\n'
+    if next == 'W': speeds = '90,' + str(180) + ',90,90\n'
+    if next == 'A': speeds = '90,90,' + str(0) + '90\n'
+    if next == 'S': speeds = '90,90,90,' + str(180) + '\n'
+    if next == 'E': speeds = str(180) + '90,90,90\n'
+    if next == 'R': speeds = '90,' + str(0) + ',90,90\n'
+    if next == 'D': speeds = '90,90,' + str(180) + '90\n'
+    if next == 'F': speeds = '90,90,90,' +str(0) + '\n'
     ser.write(str(speeds))
+    time.sleep(0.5)
+    ser.write('90,90,90,90\n')
