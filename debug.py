157645af5757fdac24e0fab8687ddaacbfcf5315
Neil Isaac <isaac.neil@gmail.com>
2014-11-15 19:47:06 -0500

    debug fixes

--- debug.py
+++ debug.py
@@ -4,18 +4,22 @@ import time
 import control
 
 ser = control.setup()
+ser.write('90,90,90,90,0\n')
 
 # Main loop
 while True:
-    next = input('Q/W/A/S for in, E/R/D/F for out ')
-    if next == 'Q': speeds = str(0) + '90,90,90\n'
-    if next == 'W': speeds = '90,' + str(180) + ',90,90\n'
-    if next == 'A': speeds = '90,90,' + str(0) + '90\n'
-    if next == 'S': speeds = '90,90,90,' + str(180) + '\n'
-    if next == 'E': speeds = str(180) + '90,90,90\n'
-    if next == 'R': speeds = '90,' + str(0) + ',90,90\n'
-    if next == 'D': speeds = '90,90,' + str(180) + '90\n'
-    if next == 'F': speeds = '90,90,90,' +str(0) + '\n'
+    next = raw_input('Q/W/A/S for in, E/R/D/F for out ')
+    next = next.upper()[0]
+
+    if next == 'Q': speeds = '80,90,90,90,0\n'
+    if next == 'W': speeds = '90,100,90,90,0\n'
+    if next == 'A': speeds = '90,90,80,90,0\n'
+    if next == 'S': speeds = '90,90,90,100,0\n'
+    if next == 'E': speeds = '100,90,90,90,0\n'
+    if next == 'R': speeds = '90,80,90,90,0\n'
+    if next == 'D': speeds = '90,90,100,90,0\n'
+    if next == 'F': speeds = '90,90,90,80,0\n'
+
     ser.write(str(speeds))
     time.sleep(0.5)
-    ser.write('90,90,90,90\n')
+    ser.write('90,90,90,90,0\n')
