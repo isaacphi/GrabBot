57076d0b468fef8fded33c0501e551e931c88e90
Neil Isaac <isaac.neil@gmail.com>
2014-11-15 20:15:55 -0500

    Fixed directions and motors in debug

--- debug.py
+++ debug.py
@@ -11,14 +11,14 @@ while True:
     next = raw_input('Q/W/A/S for in, E/R/D/F for out ')
     next = next.upper()[0]
 
-    if next == 'Q': speeds = '80,90,90,90,0\n'
-    if next == 'W': speeds = '90,100,90,90,0\n'
-    if next == 'A': speeds = '90,90,80,90,0\n'
-    if next == 'S': speeds = '90,90,90,100,0\n'
-    if next == 'E': speeds = '100,90,90,90,0\n'
-    if next == 'R': speeds = '90,80,90,90,0\n'
-    if next == 'D': speeds = '90,90,100,90,0\n'
-    if next == 'F': speeds = '90,90,90,80,0\n'
+    if next == 'W': speeds = '80,90,90,90,0\n'
+    if next == 'Q': speeds = '90,100,90,90,0\n'
+    if next == 'S': speeds = '90,90,100,90,0\n'
+    if next == 'A': speeds = '90,90,90,80,0\n'
+    if next == 'R': speeds = '100,90,90,90,0\n'
+    if next == 'E': speeds = '90,80,90,90,0\n'
+    if next == 'F': speeds = '90,90,80,90,0\n'
+    if next == 'D': speeds = '90,90,90,100,0\n'
 
     ser.write(str(speeds))
     time.sleep(0.5)
