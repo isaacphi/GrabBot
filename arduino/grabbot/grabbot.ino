c9d0f8e169b64ac5fc39081c5bd4346af1af5f83
Neil Isaac <isaac.neil@gmail.com>
2014-11-15 20:16:42 -0500

    Fix init for grabbot arduino code

--- arduino/grabbot/grabbot.ino
+++ arduino/grabbot/grabbot.ino
@@ -4,37 +4,37 @@
 /*
   Get your bot on 2014
   Reads over serial port 9600 for motor speed and grap position commands
-  in the form of 
+  in the form of
   "<NE motor speed>,<NW motor speed>,<SE motor speed>,<SW motor speed>,<grabber position>"
   values range from 0 to 180
 */
 
 Servo NE_servo, NW_servo, SE_servo, SW_servo, grab_servo;
 
-int NE_speed = 0;
-int NW_speed = 0;
-int SE_speed = 0;
-int SW_speed = 0;
-int grab_pos = 0;
+int NE_speed = 90;
+int NW_speed = 90;
+int SE_speed = 90;
+int SW_speed = 90;
+int grab_pos = 90;
 
 void setup()
-{ 
+{
   Serial.begin(9600);
   NE_servo.attach(9);  // attaches the servo on pin 9 to the servo object
-  NW_servo.attach(10); 
-  SE_servo.attach(11); 
-  SW_servo.attach(12); 
-  grab_servo.attach(13); 
-} 
- 
+  NW_servo.attach(10);
+  SE_servo.attach(11);
+  SW_servo.attach(12);
+  grab_servo.attach(13);
+}
+
 void loop()
-{ 
+{
   while (Serial.available() > 0) {
     // look for the next valid integer in the incoming serial stream,
     // separated by commas
-    NE_speed = Serial.parseInt(); 
-    NW_speed = Serial.parseInt(); 
-    SE_speed = Serial.parseInt(); 
+    NE_speed = Serial.parseInt();
+    NW_speed = Serial.parseInt();
+    SE_speed = Serial.parseInt();
     SW_speed = Serial.parseInt(); 
     grab_pos = Serial.parseInt();
     if (Serial.read() == '\n')
